ee528aadbf54658386df672664bdf832ca513b3d
Fareed Tariq <fareedtariq119@gmail.com>
2024-10-20 14:09:21 +0500

    update

--- ludo/test/widget_test.dart
+++ ludo/test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:heads_tails/main.dart';
+import 'package:dice_rolling/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
